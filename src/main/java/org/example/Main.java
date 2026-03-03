654366ec23874c8f18667325ba7715fe2ea32b24
BijonDurjoy <bijon176193@gmail.com>
2026-03-03 22:55:46 +0600

    XPath send keys and click function

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+    public static void main(String[] args) {
+       WebDriver driver = new FirefoxDriver();
+       driver.get("https://ebay.com/");
+
+       //Write something in searchbox
+        driver.findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys("Smart Phone");
+
+        //Click the search button
+        driver.findElement(By.xpath("//*[@id=\"gh-search-btn\"]")).click();
+    }
+}
